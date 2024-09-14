633039b6d911e2f547d689a38587f19373cd7608
Kenneth Poenadi <19623221@std.stei.itb.ac.id>
2024-09-14 22:59:33 +0700

    fix: some issue

--- app/dashboard/about-us/page.tsx
+++ app/dashboard/about-us/page.tsx
@@ -13,10 +13,10 @@ const About = () => {
           alt="TaskHunt Logo"
         />
         <div className={`${overlock.className} p-7 text-2xl gap-3 flex flex-col text-custom-green`}>
-        <p>
+        <p className="font-semibold">
         TaskHunt is designed with a dual mission in mind—benefiting both society and the environment. We believe that small actions can lead to big changes. By encouraging users to complete tasks like cleaning up their surroundings, TaskHunt rewards them with points that can be redeemed for exciting benefits. Our goal is to create a healthier, cleaner, and more engaged community through positive reinforcement.
         </p>
-        <p>
+        <p className="font-semibold">
         Beyond just rewarding positive actions, TaskHunt also encourages users to explore and connect with their local surroundings. Whether you're discovering hidden parks, historical landmarks, or community spaces, TaskHunt turns every task into an adventure. Through exploration, you’ll not only contribute to a better environment but also uncover the unique beauty and stories of the places around you. By fostering a sense of discovery and community, TaskHunt helps users build a deeper connection with their city and its environment.
         </p>
         </div>
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,18 +1,32 @@
-import NavBar from "./navbar";
+import Head from 'next/head';
+import NavBar from './navbar';
+
+export const metadata = {
+  title: "TaskHunt",
+  description: "Save our environment!",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <main>
-       <div className="flex flex-col h-screen md:overflow-hidden">
-      <div>
-        <NavBar/>
-      </div>
-      <div>{children}</div>
-    </div>
-    </main>
-     
+    <>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+
+      <main>
+        <div className="flex flex-col min-h-screen md:overflow-hidden">
+          <div>
+            <NavBar />
+          </div>
+          <div>{children}</div>
+        </div>
+      </main>
+    </>
   );
 }
